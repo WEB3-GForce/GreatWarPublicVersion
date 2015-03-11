5de42fc41d645b36cd1894fd3345bdd96e7c5f10
webiii <webiii@localhost>
2015-03-11 19:37:44 -0400

    Revising and testing occupiable_component.rb. Changing name to standard snake_case.

---
+++ spec/models/ecs/component/occupiable_component_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../../../spec_helper'
+
+describe OccupiableComponent do
+	it "should be able to be initialized" do
+		comp = OccupiableComponent.new
+		expect(comp.class).to equal(OccupiableComponent)
+	end
+
+	it "should have implemented to_s" do
+		comp = OccupiableComponent.new
+		expect(comp).to respond_to :to_s
+		expect(comp.to_s.class).to eq(String)
+	end
+end
