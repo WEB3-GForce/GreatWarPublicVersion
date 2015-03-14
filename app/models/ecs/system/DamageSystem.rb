d0e0a0c51261f0d4e9d9c8015ecabe821cd85a9b
Vance Zuo <vance.zuo@gmail.com>
2015-03-14 09:17:51 -0400

    Update DamageSystem to handle multiple damage comps
    Also added comments.

--- app/models/ecs/system/DamageSystem.rb
+++ app/models/ecs/system/DamageSystem.rb
@@ -1,12 +1,24 @@
+require_relative "./System.rb"
 
+=begin
+    The Damage System checks entities with health and damage
+    components, subtracting the damage represented by the damage
+    components from an entity's health as represented by its
+    health component.
 
+    The system can process multiple damage components per entity,
+    but it assumes an entity has only one health component.
+=end
 class DamageSystem
     def self.update(entity_manager)
         entity_manager.each_entity(
                 HealthComponent, DamageComponent).each { |id, components|
+
             health = components[HealthComponent].first
-            damage = components[DamageComponent].first
-            health.cur_health -= damage.amount
+
+            components[DamageComponent].each { |damage|
+                health.cur_health -= damage.amount
+            }
         }
     end
 end
