61de19b2d1a1b4998d776f9448d7ec7c8ba20c5c
David Marcano <david.marcano@yale.edu>
2015-03-22 17:48:00 -0400

    added helper functions to see if a user is logged in or not (both for dv and for test). Wrote tests to make sure a corect log in attempt works. Created Fixtures to populate the database. Log in the user after signing in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,13 @@ module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
 	end
+
+	# Returns the current logged-in user (if any).
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	def logged_in?
+		!current_user.nil?
+	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,4 +25,11 @@ class User < ActiveRecord::Base
 
 	# Enforce password length
 	validates :password, length: {minimum: 6}
+
+	# Returns the hash digest of the given string.
+	def User.digest(string)
+		cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+	                                          BCrypt::Engine.cost
+		BCrypt::Password.create(string, cost: cost)
+	end
 end
