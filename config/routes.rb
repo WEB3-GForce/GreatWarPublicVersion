d3f029d034e1173931bd208d027d9ec95da40555
David Marcano <david.marcano@yale.edu>
2015-03-22 16:30:38 -0400

    Added a Sessions controller for logging in and out. Added integration test for logging in

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   get 'users/new'
 
   get 'users/show'
@@ -8,5 +10,8 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout'=> 'sessions#destroy'
   resources :users
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,11 +4,7 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
-  end
-
-  test "should get show" do
-    get :show
-    assert_response :success
+    assert_select "title", "Sign up | The Great War"
   end
 
 end
