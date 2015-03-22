7b21432da6761e9f881fc72e1ef0453e1f2afc52
webiii <webiii@localhost>
2015-03-22 14:38:21 -0400

    Added EnergyComponent.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,6 +24,7 @@ require_relative '../app/models/ecs/system/motion_system.rb'
 require_relative '../app/models/ecs/system/kill_system.rb'
 require_relative '../app/models/ecs/system/damage_system.rb'
 require_relative '../app/models/ecs/system/range_system.rb'
+require_relative '../app/models/ecs/system/turn_system.rb'
 
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
