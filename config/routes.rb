20f1a6e4c3fe6ca11077544812112ed5ae29509d
David Marcano <david.marcano@yale.edu>
2015-03-22 15:39:26 -0400

    added user controller, made a partial for error messages when signing up, added corresponding user views. Fixed the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/show'
+
   root             'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
