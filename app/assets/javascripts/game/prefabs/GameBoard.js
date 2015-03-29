53b5107717fe2730b9bc012b188951d72f27f11d
Marvin Qian <fueledbymarvin@gmail.com>
2015-03-29 17:19:24 -0400

    A few minor changes to highlighting
    Created a mapping between highlight type and the associated index
    (currently just colors but later it could be something like movement,
    attack, etc.). I also added an unhighlight all function since that's
    how we'll most often be unhighlighting anyway.

--- app/assets/javascripts/game/prefabs/GameBoard.js
+++ app/assets/javascripts/game/prefabs/GameBoard.js
@@ -1,5 +1,11 @@
 'use strict';
 
+var HIGHLIGHT_TYPES = {
+    blue: 51,
+    red: 52,
+    green: 53
+}
+
 var GameBoard = function(game) {
     Phaser.Tilemap.call(this, game, 'tileset');
 
@@ -14,6 +20,10 @@ var GameBoard = function(game) {
     this.highlightLayer = this.createLayer('highlightLayer');
 
     this.drawGrid();
+    this.unhighlightAll();
+    this.highlight(0, 0, "blue");
+    this.highlight(0, 1, "red");
+    this.highlight(0, 2, "green");
 };
 
 GameBoard.prototype = Object.create(Phaser.Tilemap.prototype);
@@ -28,17 +38,21 @@ GameBoard.prototype.revealFog = function(x, y) {
     this.removeTile(x, y, this.fogLayer);
 }
 
-GameBoard.prototype.highlight = function(x, y) {
-    var highlightIndex = 51; 
-    this.putTile(highlightIndex, x, y, this.highlightLayer);
+GameBoard.prototype.highlight = function(x, y, type) {
+    this.putTile(HIGHLIGHT_TYPES[type], x, y, this.highlightLayer);
 }
 GameBoard.prototype.unhighlight = function(x, y) {
     this.removeTile(x, y, this.highlightLayer);
 }
+GameBoard.prototype.unhighlightAll = function() {
+    for (var i = 0; i < this.width; i++)
+	for (var j = 0; j < this.height; j++)
+	    this.unhighlight(i, j);
+}
 
 GameBoard.prototype.drawGrid = function() {
     this.grid = this.game.add.graphics();
-    this.grid.lineStyle(1, 0xA9A9A9, 0.5);
+    this.grid.lineStyle(1, 0x000000, 0.1);
     // draw vertical lines:
     for (var x = 0; x < this.width * 32; x += 32) {
     	this.grid.moveTo(x, 0);
