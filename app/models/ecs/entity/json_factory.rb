6a4ebd808f79c09517d94b41d603c06b73ccc115
webiii <webiii@localhost>
2015-03-30 13:30:26 -0400

    Adding Json methods for all entity types, the board, and initial game data

--- app/models/ecs/entity/json_factory.rb
+++ app/models/ecs/entity/json_factory.rb
@@ -2,56 +2,242 @@
 #Dir[File.dirname(__FILE__) + '/../system/*.rb'].each {|file| require_relative file }
 #Dir[File.dirname(__FILE__) + '/./*.rb'].each {|file| require_relative file }
 =begin
-
+	The JsonFactory is the one stop shop for all things json. Have some actions
+	or entities to send to the frontend? JsonFactory has you covered. It will
+	handle both sending newly created entities as well as update actions like
+	movement and attack to the frontend.
+	
+	Note: It is the responsibility of the caller to ensure that the entities
+	are well-formed.
 =end
 class JsonFactory
 
-	def self.board_json(entity_manager)
-	
-		json = Hash.new
-		json["response"] = "board"
+	# Converts a square entity into a hash object.
+	#
+	# Arguments
+	#   entity_manager = the manager in which the entity is kept.
+	#   entity         = the square entity to be jsoned
+	#
+	# Returns
+	#   A hash that is ready to be jsoned
+	def self.square(entity_manager, entity)
+		terrain_comp = entity_manager.get_components(entity, TerrainComponent).first
+		return {"id"      => entity,
+		        "terrain" => terrain_comp.type.to_s}
+	end
+
+
+	# This converts a square entity into a json-ready hash. In particular,
+	# this will be used for requests such as returning the path of a movement
+	# which don't need to tell all the information about a square but
+	# simply a way to identify it. Both the id and its x and y coordinates
+	# are provided.
+	#
+	# Argumetns
+	#   entity_manager = the manager in which the entity is kept.
+	#   entity         = the square entity to be jsoned
+	#
+	# Returns
+	#   A hash that is ready to be jsoned
+	def self.square_path(entity_manager, entity)
+		pos_comp = entity_manager.get_components(entity, PositionComponent).first
+		return {"id" => entity,
+		        "row"  => pos_comp.row,
+		        "col"  => pos_comp.col}
+	end
+
+	# Converts a player entity into a hash object.
+	#
+	# Argumetns
+	#   entity_manager = the manager in which the entity is kept.
+	#   entity         = the player entity to be jsoned
+	#
+	# Returns
+	#   A hash that is ready to be jsoned
+	def self.player(entity_manager, entity)
+		name_comp = entity_manager.get_components(entity, NameComponent).first
+
+		ai_comp = entity_manager.get_components(entity, AIComponent).first
+		player_type = "CPU" if ai_comp	
+
+		human_comp = entity_manager.get_components(entity, HumanComponent).first
+		player_type = "Human" if human_comp
+
+		return {"id"      => entity,
+		        "name"    => name_comp.name,
+		        "type"    => player_type}
+	end
+
+	# Converts a turn entity into a hash object.
+	#
+	# Argumetns
+	#   entity_manager = the manager in which the entity is kept.
+	#   entity         = the turn entity to be jsoned
+	#
+	# Returns
+	#   A hash that is ready to be jsoned
+	def self.turn(entity_manager, entity)
+		turn_comp = entity_manager.get_components(entity, TurnComponent).first
+		return {"id"      => entity,
+		        "current" => turn_comp.current_turn}
+	end
+
+	# This method is responsible for converting a piece entity into a json-
+	# ready hash. In short, a piece is any element that a player can control
+	# whether it be an artillery or command_bunker.
+	#
+	# This method handles all possible pieces (and hence makes it easier to
+	# add and delete components from a given piece)
+	#
+	# Argumetns
+	#   entity_manager = the manager in which the entity is kept.
+	#   entity         = the piece entity to be jsoned
+	#
+	# Returns
+	#   A hash that is ready to be jsoned
+	def self.piece(entity_manager, entity)
+		piece_hash       = Hash.new
+		piece_hash["id"] = entity
+		
+		piece_comp = entity_manager.get_components(entity, PieceComponent).first
+		piece_hash["type"] = piece_comp.type.to_s
+
+		owned_comp = entity_manager.get_components(entity, OwnedComponent).first
+		piece_hash["owner"] = owned_comp.owner
+
+		pos_comp = entity_manager.get_components(entity, PositionComponent).first
+		piece_hash["position"] = {"row" => pos_comp.row,
+		                          "col" => pos_comp.col}
+
+		health_comp = entity_manager.get_components(entity, HealthComponent).first
+		piece_hash["health"] = {"current" => health_comp.cur_health,
+		                        "max"     => health_comp.max_health}
+
+		energy_comp = entity_manager.get_components(entity, EnergyComponent).first
+		if energy_comp
+		   piece_hash["energy"] = {"current" => energy_comp.cur_energy,
+		                           "max"     => energy_comp.max_energy}
+		end
+
+		motion_comp = entity_manager.get_components(entity, MotionComponent).first
+		if motion_comp
+		   piece_hash["motion"] = {"cost" => motion_comp.energy_cost}
+		end
+
+		melee_comp = entity_manager.get_components(entity, MeleeAttackComponent).first
+		if melee_comp
+		   piece_hash["melee"] = {"attack" => melee_comp.attack,
+		                          "cost"   => melee_comp.energy_cost}
+		end
+
+		range_comp = entity_manager.get_components(entity, RangeAttackComponent).first
+		piece_hash["range"] = Hash.new
+		if range_comp
+		   piece_hash["range"] = {"attack" => range_comp.attack,
+		                          "range"  => {"min" => range_comp.min_range,
+		                                       "max" => range_comp.max_range},
+		                          "splash" => range_comp.splash.size,
+		                          "cost"   => range_comp.energy_cost}
+		end
+
+		range_immune_comp = entity_manager.get_components(entity, RangeAttackImmunityComponent).first
+		piece_hash["range"]["immune"] = range_immune_comp != nil
+		return piece_hash
+	end
+
+	# Converts the board into a json-ready hash. This method is particularly
+	# useful for initialization of the frontend and sending the frontend the
+	# data for the board.
+	#
+	# Argumetns
+	#   entity_manager = the manager that contains the board
+	#
+	# Returns
+	#   A hash that is ready to be jsoned
+	def self.board(entity_manager)
 		board_array = []
 		(0...entity_manager.row).each { |row|
 			(0...entity_manager.col).each { |col|
-				board_array.push self.square_json(entity_manager, entity_manager.board[row][col][0])				
+				board_array.push self.square(entity_manager,
+					entity_manager.board[row][col][0])				
 			}
 		}
-		json["board"] = board_array
-		json
+		return {"row"      => entity_manager.row,
+		        "col"      => entity_manager.col,
+		        "squares"  => board_array}
 	end
+
+
+	# This method is responsible for sending all relevant game
+	# start data to the frontend. Once the frontend receives this, it will
+	# be able to completely initialize the browser for a new game.
+	#
+	# Arguments
+	#   entity_manager = the manager of the entities
+	#   players        = an array of player entities
+	#   turn           = the turn entity denoting whose turn it is.
+	#   pieces         = an array of all the pieces in the game
+	#
+	# Returns
+	#   A hash that is ready to be jsoned
+	def self.game_start(entity_manager, players, turn, pieces)
+		player_array = []
+		players.each { |player|
+			player_array.push self.player(entity_manager, player)
+		}
 	
-	def self.square_json(entity_manager, entity)
-		terrain_comp = entity_manager.get_components(entity, TerrainComponent).first
-		json = Hash.new
-		json["terrain"] = terrain_comp.type.to_s
-		json
+		turn_hash = self.turn(entity_manager, turn)
+		board     = self.board(entity_manager)
+		
+		piece_array = []
+		pieces.each { |piece|
+			piece_array.push self.piece(entity_manager, piece)
+		}
+
+		return {"response" => "game_start",
+		        "board"    => board,
+		        "players"  => player_array,
+		        "turn"     => turn_hash,
+		        "pieces"   => piece_array}
 	end
 
-	def self.path_json(entity_manager, path)
-		json = Hash.new
-		json["response"] = "moveable_locations"
+
+	# This returns the results of a move command to the frontend. It specifies
+	# the entity that moved along with the path it moved upon.
+	#
+	# Argumetns
+	#   entity_manager = the manager that contains the entities
+	#   moving_entity  = the entity that moved.
+	#   path           = an array of square entities denoting the path of motion.
+	#
+	# Returns
+	#   A hash that is ready to be jsoned
+	def self.move(entity_manager, moving_entity, path)
 		path_array = []
-		path.each { |path_square|
-			path_array.push self.square_position_json(entity_manager, path_square)
+		path.each { |square|
+			path_array.push self.square_path(entity_manager, square)
 		}
-		json["path"] = path_array
+		return {"response" => "move",
+		        "mover"    => moving_entity,
+		        "path"     => path_array}
 	end
 
-	def self.moveable_locations_json(entity_manager, locations)
-		json = Hash.new
-		json["response"] = "moveable_locations"
+	def self.moveable_locations(entity_manager,  moving_entity, locations)
 		locations_array = []
-		locations.each { |path_square|
-			locations_array.push self.square_position_json(entity_manager, path_square)
+		locations.each { |square|
+			locations_array.push self.square_path(entity_manager, square)
 		}
-		json["locations"] = locations_array
+		return {"response"  => "moveable locations",
+		        "mover"     => moving_entity,
+		        "locations" => locations_array}
 	end
 
-
-	def self.square_position_json(entity_manager, entity)
-		pos_comp = entity_manager.get_components(entity, PositionComponent).first
-		[pos_comp.x, pos_comp.y]
-	end
+	# Actions to handle:
+	#   Attack
+	#   Attackable locations
+	#   Turn end
+	#   Player finished
+	#   Game over
 end
 
 #{ response: "board"
---
+++ spec/models/ecs/entity/json_factory_spec.rb
@@ -0,0 +1,285 @@
+require_relative '../../../spec_helper'
+
+describe JsonFactory do
+
+	let(:row)               {3}
+	let(:col)               {3}
+	let(:manager)           {EntityManager.new(row, col)}
+	let(:human1)            {EntityFactory.human_player(manager, "David")}
+	let(:ai)                {EntityFactory.ai_player(manager, "R.O.B")}
+	let(:infantry)          {EntityFactory.infantry(manager, human1)}
+	let(:machine_gun)       {EntityFactory.machine_gun(manager, human1)}
+	let(:artillery)         {EntityFactory.artillery(manager, human1)}	
+	let(:command_bunker)    {EntityFactory.command_bunker(manager, human1)}
+	let(:flatland00)        {EntityFactory.flatland_square(manager)}
+	let(:river01)           {EntityFactory.river_square(manager)}
+	let(:mountain02)        {EntityFactory.mountain_square(manager)}
+	let(:flatland10)        {EntityFactory.flatland_square(manager)}
+	let(:flatland11)        {EntityFactory.flatland_square(manager)}
+	let(:flatland12)        {EntityFactory.flatland_square(manager)}
+	let(:flatland20)        {EntityFactory.flatland_square(manager)}
+	let(:flatland21)        {EntityFactory.flatland_square(manager)}
+	let(:flatland22)        {EntityFactory.flatland_square(manager)}
+        let(:square_array)      {[flatland00, river01,    mountain02,
+		                  flatland10, flatland11, flatland12,
+		                  flatland20, flatland21, flatland22]}
+	let(:turn)              {EntityFactory.turn_entity(manager, [human1, ai])}
+
+	def set_simple
+		array = square_array.dup   
+		(0...manager.row).each { |row|
+			(0...manager.col).each { |col|
+				square = array.shift
+				manager.add_component(square,
+					PositionComponent.new(row, col))
+				manager.board[row][col] = [square, []]
+			}
+		}             
+	end
+
+	def set_intermediate
+		set_simple()
+		manager.board[0][0][1].push infantry
+		manager.add_component(infantry, PositionComponent.new(0, 0))
+		manager.board[1][1][1].push machine_gun
+		manager.add_component(machine_gun, PositionComponent.new(1, 1))
+		manager.board[2][2][1].push artillery
+		manager.add_component(artillery, PositionComponent.new(2, 2))
+		manager.board[2][0][1].push command_bunker
+		manager.add_component(command_bunker, PositionComponent.new(2, 0))
+	end
+
+	context "when calling square" do
+		it "should return a hash with the attributes of the square" do
+			expect(JsonFactory.square(manager, flatland00)).to eq(
+				{"id" => flatland00, "terrain" => "flatland"})
+			expect(JsonFactory.square(manager, river01)).to eq(
+				{"id" => river01, "terrain" => "river"})
+			expect(JsonFactory.square(manager, mountain02)).to eq(
+				{"id" => mountain02, "terrain" => "mountain"})
+		end
+	end
+
+	context "when calling player" do
+		it "should return a hash for a human" do
+			expect(JsonFactory.player(manager, human1)).to eq(
+				{"id" => human1, "name" => "David",
+				 "type" => "Human"})
+		end
+
+		it "should return a hash for an ai" do
+			expect(JsonFactory.player(manager, ai)).to eq(
+				{"id" => ai, "name" => "R.O.B",
+				 "type" => "CPU"})
+		end
+	end
+
+	context "when calling turn" do
+		it "should return a hash for a turn entity" do
+			expect(JsonFactory.turn(manager, turn)).to eq(
+				{"id" => turn, "current" => human1})
+				
+			manager.get_components(turn, TurnComponent).first.next_turn
+			expect(JsonFactory.turn(manager, turn)).to eq(
+				{"id" => turn, "current" => ai})
+		end
+	end
+
+
+	context "when calling square_path" do
+		it "should return a hash with the attributes of the square" do
+			set_simple
+			expect(JsonFactory.square_path(manager, flatland00)).to eq(
+				{"id" => flatland00, "row" => 0, "col" => 0})
+			expect(JsonFactory.square_path(manager, river01)).to eq(
+				{"id" => river01, "row" => 0, "col" => 1})
+			expect(JsonFactory.square_path(manager, mountain02)).to eq(
+				{"id" => mountain02, "row" => 0, "col" => 2})
+		end
+	end
+
+	context "when calling piece" do
+		it "should return a proper hash for an infantry" do
+			set_intermediate
+			piece_comp  = manager.get_components(infantry, PieceComponent).first
+			pos_comp    = manager.get_components(infantry, PositionComponent).first
+			health_comp = manager.get_components(infantry, HealthComponent).first
+			energy_comp = manager.get_components(infantry, EnergyComponent).first
+			motion_comp = manager.get_components(infantry, MotionComponent).first
+			melee_comp  = manager.get_components(infantry, MeleeAttackComponent).first
+			range_comp  = manager.get_components(infantry, RangeAttackComponent).first
+			answer = {"id"     => infantry,
+			          "type"   => PieceComponent.infantry.type.to_s,
+			          "owner"  => human1,
+			          "position" => {"row" => pos_comp.row,
+			                         "col" => pos_comp.col},
+			          "health" => {"current" => health_comp.cur_health,
+			                       "max"     => health_comp.max_health},
+			          "energy" => {"current" => energy_comp.cur_energy,
+			                       "max"     => energy_comp.max_energy},
+			          "motion" => {"cost"    => motion_comp.energy_cost},	
+			          "melee"  => {"attack"  => melee_comp.attack,
+			                       "cost"    => melee_comp.energy_cost},
+			          "range"  => {"attack"  => range_comp.attack,
+			                       "range"   => {"min" => range_comp.min_range,
+			                                     "max" => range_comp.max_range},
+			                       "splash"  => range_comp.splash.size,
+			                       "cost"    => range_comp.energy_cost,
+			                       "immune"  => false}
+			        }	                      
+		
+			expect(JsonFactory.piece(manager, infantry)).to eq(answer)
+		end
+
+		it "should return a proper hash for a machine_gun" do
+			set_intermediate
+			piece_comp  = manager.get_components(machine_gun, PieceComponent).first
+			pos_comp    = manager.get_components(machine_gun, PositionComponent).first
+			health_comp = manager.get_components(machine_gun, HealthComponent).first
+			energy_comp = manager.get_components(machine_gun, EnergyComponent).first
+			motion_comp = manager.get_components(machine_gun, MotionComponent).first
+			melee_comp  = manager.get_components(machine_gun, MeleeAttackComponent).first
+			range_comp  = manager.get_components(machine_gun, RangeAttackComponent).first
+			answer = {"id"     => machine_gun,
+			          "type"   => PieceComponent.machine_gun.type.to_s,
+			          "owner"  => human1,
+			          "position" => {"row" => pos_comp.row,
+			                         "col" => pos_comp.col},
+			          "health" => {"current" => health_comp.cur_health,
+			                       "max"     => health_comp.max_health},
+			          "energy" => {"current" => energy_comp.cur_energy,
+			                       "max"     => energy_comp.max_energy},
+			          "motion" => {"cost"    => motion_comp.energy_cost},	
+			          "melee"  => {"attack"  => melee_comp.attack,
+			                       "cost"    => melee_comp.energy_cost},
+			          "range"  => {"attack"  => range_comp.attack,
+			                       "range"   => {"min" => range_comp.min_range,
+			                                     "max" => range_comp.max_range},
+			                       "splash"  => range_comp.splash.size,
+			                       "cost"    => range_comp.energy_cost,
+			                       "immune"  => false}
+			        }	                      
+		
+			expect(JsonFactory.piece(manager, machine_gun)).to eq(answer)
+		end
+
+		it "should return a proper hash for an artillery" do
+			set_intermediate
+			piece_comp  = manager.get_components(artillery, PieceComponent).first
+			pos_comp    = manager.get_components(artillery, PositionComponent).first
+			health_comp = manager.get_components(artillery, HealthComponent).first
+			energy_comp = manager.get_components(artillery, EnergyComponent).first
+			motion_comp = manager.get_components(artillery, MotionComponent).first
+			melee_comp  = manager.get_components(artillery, MeleeAttackComponent).first
+			range_comp  = manager.get_components(artillery, RangeAttackComponent).first
+			answer = {"id"     => artillery,
+			          "type"   => PieceComponent.artillery.type.to_s,
+			          "owner"  => human1,
+			          "position" => {"row" => pos_comp.row,
+			                         "col" => pos_comp.col},
+			          "health" => {"current" => health_comp.cur_health,
+			                       "max"     => health_comp.max_health},
+			          "energy" => {"current" => energy_comp.cur_energy,
+			                       "max"     => energy_comp.max_energy},
+			          "motion" => {"cost"    => motion_comp.energy_cost},
+			          "range"  => {"attack"  => range_comp.attack,
+			                       "range"   => {"min" => range_comp.min_range,
+			                                     "max" => range_comp.max_range},
+			                       "splash"  => range_comp.splash.size,
+			                       "cost"    => range_comp.energy_cost,
+			                       "immune"  => false}
+			        }	                      
+		
+			expect(JsonFactory.piece(manager, artillery)).to eq(answer)
+		end
+
+		it "should return a proper hash for an command_bunker" do
+			set_intermediate
+			piece_comp  = manager.get_components(command_bunker, PieceComponent).first
+			pos_comp    = manager.get_components(command_bunker, PositionComponent).first
+			health_comp = manager.get_components(command_bunker, HealthComponent).first
+			energy_comp = manager.get_components(command_bunker, EnergyComponent).first
+			melee_comp  = manager.get_components(command_bunker, MeleeAttackComponent).first
+			range_comp  = manager.get_components(command_bunker, RangeAttackComponent).first
+			answer = {"id"     => command_bunker,
+			          "type"   => PieceComponent.command_bunker.type.to_s,
+			          "owner"  => human1,
+			          "position" => {"row" => pos_comp.row,
+			                         "col" => pos_comp.col},
+			          "health" => {"current" => health_comp.cur_health,
+			                       "max"     => health_comp.max_health},
+			          "energy" => {"current" => energy_comp.cur_energy,
+			                       "max"     => energy_comp.max_energy},
+			          "range"  => {"immune"  => true}
+			        }	                      
+		
+			expect(JsonFactory.piece(manager, command_bunker)).to eq(answer)
+		end
+	end
+
+
+	context "when calling board" do
+		it "should return a hash of the board" do
+			set_simple
+			board_array = []
+			square_array.each { |square|
+				board_array.push JsonFactory.square(manager, square)
+			}
+			expect(JsonFactory.board(manager)).to eq(
+				{"row" => row, "col" => col, "squares" => board_array})
+		end
+	end
+
+
+	context "when calling game_start" do
+		it "should return a hash of the game_start" do
+			set_intermediate
+			players = [human1, ai]
+			player_array = []
+			players.each { |player|
+				player_array.push JsonFactory.player(manager, player)
+			}
+			pieces = [infantry, machine_gun, artillery, command_bunker]
+			pieces_array = []
+			pieces.each { |piece|
+				pieces_array.push JsonFactory.piece(manager, piece)
+			}
+
+			expect(JsonFactory.game_start(manager, players, turn, pieces)).to eq(
+				{"response" => "game_start", 
+				 "board"    => JsonFactory.board(manager),
+				 "players"  => player_array,
+				 "turn"     => JsonFactory.turn(manager, turn),
+				 "pieces"   => pieces_array})
+		end
+	end
+
+	context "when calling move" do
+		it "should return a hash of a move action" do
+			set_simple
+			path = [flatland20, flatland10, flatland00]
+			path_array = []
+			path.each { |square|
+				path_array.push JsonFactory.square_path(manager, square)
+			}
+			expect(JsonFactory.move(manager, infantry, path)).to eq(
+				{"response" => "move", "mover" => infantry, 
+			 	 "path" => path_array})
+		end
+	end
+
+
+	context "when calling moveable locations" do
+		it "should return a hash of a json for a moveable locations request" do
+			set_simple
+			locations = []
+			square_array.each { |square|
+				locations.push JsonFactory.square_path(manager, square)
+			}
+			expect(JsonFactory.moveable_locations(manager, machine_gun, square_array)).to eq(
+				{"response" => "moveable locations", "mover" => machine_gun, 
+			 	 "locations" => locations})
+		end
+	end
+
+end
