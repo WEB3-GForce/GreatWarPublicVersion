dae719afdcc85e14ed82c979a9f9cffcda14a954
webiii <webiii@localhost>
2015-03-31 16:28:21 -0400

    Update Game.rb do move methods

--- app/models/ecs/Game.rb
+++ app/models/ecs/Game.rb
@@ -1,78 +1,105 @@
 require_relative "./entity/entity_factory.rb"
 
 class Game
-    attr_reader :entity_manager
 
-    def initialize(rows=30, cols=30, player_names=["Player 1", "Player 2"])
-        @entity_manager = EntityManager.new(rows, cols)
-        @start_info = EntityFactory.create_game_basic(@entity_manager, player_names)
+    def init_game(rows=30, cols=30, player_names=["Player 1", "Player 2"])
+        manager = EntityManager.new(rows, cols)
+        players, turn, pieces = EntityFactory.create_game_basic(manager, player_names)
+        start_json = JsonFactory.game_start(manager, players, turn, pieces)
+        return manager, start_json
     end
 
-    def get_start_info
-        players, turn, pieces = @start_info
-        return JsonFactory.game_start(@entity_manager, players, turn, pieces)
+    def get_full_info(requester, em, row, col)
+    	return { "tile" => self.get_tile_info(requester, em, row, col),
+    		 "unit" => self.get_unit_info(requester, em, row, col) }
     end
 
-    def get_full_info(requester, row=nil, col=nil)
-        # return combination of get_tile_info, get_unit_info, get_player_info
+    def get_tile_info(requester, em, row, col)        
+        return JsonFactory.square(em, em.board[row][col][0])
     end
 
-    def get_tile_info(requester, row=nil, col=nil)        
-        # return @entity_manager.board[row][col][0]
+    def get_unit_info(requester, em, row, col)
+        entity = em.board[row][col][1].first
+        return JsonFactory.piece(em, entity)
     end
 
-    def get_unit_info(requester, row=nil, col=nil)
-        unit = @entity_manager.board[row][col][1].first
-        return nil
-    end
-
-    def get_player_info(requester, name=nil)
-        @entity_manager.each_entity(NameComponent) { |entity| 
-            nameComp = @entity_manager[entity][NameComponent].first
-            return self.player(@entity_manager, entity) if name == nameComp.name
+    def get_player_info(requester, em, name=nil)
+        em.each_entity(NameComponent) { |entity| 
+            nameComp = em[entity][NameComponent].first
+            return JsonFactory.player(em, entity) if name == nameComp.name
         }
         return []
     end
 
-    def get_all_full_info(requester)
+    def get_all_full_info(em, requester)
+    	#(0..em.row).each 
     end
 
-    def get_all_tile_info(requester)
+    def get_all_tile_info(em, requester)
     end
 
-    def get_all_unit_info(requester)
+    def get_all_unit_info(em, requester)
     end
 
-    def get_all_player_info(requester)
-    end
-
-
-    def get_unit_moves(requester, row=nil, col=nil)
-    end
-
-    def get_unit_melee_attacks(requester, row=nil, col=nil)
-    end
-
-    def get_unit_range_attacks(requester, row=nil, col=nil)
-    end
-
-    def get_all_unit_moves(requester, row=nil, col=nil)
-    end
-
-    def get_all_unit_melee_attacks(requester, row=nil, col=nil)
-    end
-
-    def get_all_unit_range_attacks(requester, row=nil, col=nil)
+    def get_all_player_info(em, requester)
     end
 
 
-    def do_move(requester, entity, row=nil, col=nil)
+    def get_unit_moves(requester, em, row, col)
+    	entity = em.board[row][col][1].first
+    	return self.get_unit_moves_entity(requester, em, entity)
+    end
+    
+    def get_unit_entity_moves(requester, em, entity)
+    	locations = MotionSystem.moveable_locations(em, entity)
+    	return EntityFactory.moveable_locations(em, entity, locations)
     end
 
-    def do_melee_attack(requester, attacker_entity, target_entity)
+    def do_unit_move(requester, em, unit_row, unit_col, row, col)
+    	entity = em.board[unit_row][unit_col][1].first
+    	return self.do_move_entity(requester, em, entity, row, col)
     end
 
-    def do_ranged_attack(requester, attacker_entity, target_entity)
+    def do_entity_unit_move(requester, em, entity, row, col)
+        entity_location = em.board[row][col][0]
+        return self.do_move_entity2(requester, em, entity, entity_location)
+    end
+    
+    def do_entity_unit_move_entity(requester, em, entity, entity_location)
+    	path = MotionSystem.make_move(em, entity, entity_location)
+    	return JsonFactory.move(em, entity, path)
+    end
+
+
+    def get_unit_melee_attacks(requester, em, row, col)
+    	entity = em.board[row][col][1].first
+    end
+    
+    def get_unit_entity_melee_attacks(requester, em, row, col)
+    	entity = em.board[row][col][1].first
+    end
+    
+
+    def get_unit_range_attacks(requester, em, row, col)
+    end
+    
+
+    def get_all_unit_moves(requester, em, row, col)
+    end
+    
+
+    def get_all_unit_melee_attacks(requester, em, row, col)
+    end
+    
+
+    def get_all_unit_range_attacks(requester, em, row, col)
+    end
+
+
+    def do_melee_attack(requester, em, attacker_entity, target_entity)
+    end
+
+    def do_ranged_attack(requester, em, attacker_entity, target_entity)
     end
 
 end
