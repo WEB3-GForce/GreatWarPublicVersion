b1122b866bc44a58ea9b9f7f71cd22fbc697b275
Benjamin Eskildsen <benjamin.eskildsen@yale.edu>
2015-04-12 14:16:24 -0400

    merged Marvin's changes in and tried to get socket_controller back how it was

--- app/controllers/socket_controller.rb
+++ app/controllers/socket_controller.rb
@@ -8,73 +8,74 @@ class SocketController < WebsocketRails::BaseController
     method_name = message['action']
     method_params = message['arguments']
 
-    # req_id = 0
-    # game_id = 0
+    req_id = 0
+    game_id = 0
 
-    # # Should separate initialization call to backend from that to frontend,
-    # # since we only need to call it once on backend, but multiple times on
-    # # frontend.
-    # if method_name == 'init_game'
-    #     em, response = Game.init_game(*method_params)
+    # Should separate initialization call to backend from that to frontend,
+    # since we only need to call it once on backend, but multiple times on
+    # frontend.
+    if method_name == 'init_game'
+        em, response = Game.init_game(*method_params)
 
-    #     @@game[game_id] = em if !em.nil?
+        @@game[game_id] = em if !em.nil?
 
-    # elsif Game.respond_to? method_name      
-    #     manager = @@game[game_id]
-    #     method_params.unshift manager
-    #     method_params.unshift req_id
+    elsif Game.respond_to? method_name      
+        manager = @@game[game_id]
+        method_params.unshift manager
+        method_params.unshift req_id
         
-    #     response = Game.public_send(method_name, *method_params)
-    #     p response
-    # end
+        response = Game.public_send(method_name, *method_params)
+        p response
+    end
 
     # # the front end expects to response to be an array, if it's not though,
     # # that's fine, it just needs to be sent as one regardless, hence this:
-    # response = [response] unless response.kind_of?(Array)
-    if method_name == 'init_game'
-      response = [
-                  {
-                    action: "revealUnit",
-                    arguments: [{
-                                  id: 1,
-                                  type: 'infantry',
-                                  x: 5,
-                                  y: 5,
-                                  player: 'test',
-                                  stats: {
-                                    health: {
-                                      current: 10,
-                                      max: 10
-                                    },
-                                    energy: {
-                                      current: 7,
-                                      max: 10
-                                    },
-                                    range: {
-                                      attack: 10
-                                    }
-                                  }
-                                }]
-                  },
-                  {
-                    action: "updateUnitsHealth",
-                    arguments: [[
-                                 {
-                                   id: 1,
-                                   newHealth: 4
-                                 }
-                                ]]
-                  }
+    response = [response] unless response.kind_of?(Array)
+    
+    # if method_name == 'init_game'
+    #   response = [
+    #               {
+    #                 action: "revealUnit",
+    #                 arguments: [{
+    #                               id: 1,
+    #                               type: 'infantry',
+    #                               x: 5,
+    #                               y: 5,
+    #                               player: 'test',
+    #                               stats: {
+    #                                 health: {
+    #                                   current: 10,
+    #                                   max: 10
+    #                                 },
+    #                                 energy: {
+    #                                   current: 7,
+    #                                   max: 10
+    #                                 },
+    #                                 range: {
+    #                                   attack: 10
+    #                                 }
+    #                               }
+    #                             }]
+    #               },
+    #               {
+    #                 action: "updateUnitsHealth",
+    #                 arguments: [[
+    #                              {
+    #                                id: 1,
+    #                                newHealth: 4
+    #                              }
+    #                             ]]
+    #               }
                   
-                 ]
-    elsif method_name == "get_unit_actions"
-      response = [
-                  {
-                    action: "showUnitActions",
-                    arguments: [["ranged", "melee", "move"]]
-                  }
-                 ]
-    end
+    #              ]
+    # elsif method_name == "get_unit_actions"
+    #   response = [
+    #               {
+    #                 action: "showUnitActions",
+    #                 arguments: [["ranged", "melee", "move"]]
+    #               }
+    #              ]
+    # end
 
     p method_name
 
