3d97b2f93a1bcc9f7ed6b1387ae8d2b5295ae5f4
webiii <webiii@localhost>
2015-04-14 19:42:02 -0400

    Fixing motion system.

--- app/models/ecs/system/motion_system.rb
+++ app/models/ecs/system/motion_system.rb
@@ -258,8 +258,11 @@ public
 		end
 		
 		motion_comp = entity_manager.get_components(entity, MotionComponent).first
+		energy_comp = entity_manager.get_components(entity, EnergyComponent).first
 		pos_comp    = entity_manager.get_components(entity, PositionComponent).first
 		own_comp    = entity_manager.get_components(entity, OwnedComponent).first
+		
+		motion_comp.max_movement = energy_comp.cur_energy / motion_comp.cost
 		
 		result = []
 		self.determine_locations(entity_manager, own_comp.owner, pos_comp.row,
@@ -300,8 +303,11 @@ public
 		end
 
 		motion_comp = entity_manager.get_components(entity, MotionComponent).first
+		energy_comp = entity_manager.get_components(entity, EnergyComponent).first
 		pos_comp    = entity_manager.get_components(entity, PositionComponent).first
 		own_comp    = entity_manager.get_components(entity, OwnedComponent).first
+		
+		motion_comp.max_movement = energy_comp.cur_energy / motion_comp.cost
 		
 		path = self.determine_path(entity_manager, own_comp.owner, pos_comp.row,
 			pos_comp.col, end_pos.row, end_pos.col, motion_comp.max_movement, [])
